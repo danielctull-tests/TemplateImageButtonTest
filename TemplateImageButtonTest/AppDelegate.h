87d105da27b057620464a9f0e73a8720e1130b46
Daniel Tull <dt@danieltull.co.uk>
2014-12-10 19:12:47 +0000

    Initial Commit

---
+++ TemplateImageButtonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TemplateImageButtonTest
+//
+//  Created by Daniel Tull on 10.12.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TemplateImageButtonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemplateImageButtonTest
+//
+//  Created by Daniel Tull on 10.12.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
